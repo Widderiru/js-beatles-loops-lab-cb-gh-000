4915f5d70ee62419e5480f4bc41410462e1cfeee
Miriam Wagner <miriam.wagner@wagner-dn.de>
2019-01-30 12:52:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var array = [];
+  for (counter = 0; counter ++, counter < musicians.length) {
+    array.push(musicians[counter] + " plays "+ instruments[counter]);
+  }
+  return array;
+}
